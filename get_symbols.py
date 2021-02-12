f28df024c1c6ed6a5a7c464c480d4cf93beeff87
Shun Cheung <scheung@loyalty.com>
2021-02-12 01:10:04 -0500

    feat(get_symbols): init

---
+++ get_symbols.py
@@ -0,0 +1,7 @@
+
+def get_symbols():
+    # getting it from https://www.advfn.com/nasdaq/nasdaq.asp
+    return ['AAL', 'AAPL', 'ACIU', 'ACST', 'ADTX', 'ADXS', 'AEZS', 'AGTC', 'AIKI', 'AMD', 'APHA', 'ASRT', 'ATOS', 'AYRO', 'AZN', 'BIOL', 'BMBL', 'BNGO', 'BOXL', 'BRQS', 'CAN', 'CGC', 'CIDM', 'CKPT', 'CLBS', 'CLOV', 'CLSN', 'COMS', 'CRBP', 'CRNT', 'CRON', 'CSCO', 'CSCW', 'CTRM', 'DKNG', 'DOYU', 'EBON', 'FCEL', 'FUTU', 'GEVO', 'GHSI', 'GLUU', 'GNUS', 'HUGE', 'IDEX', 'IFMK', 'INFI', 'INPX', 'INTC', 'IRDM', 'ITRM', 'JAGX', 'KHC', 'KXIN', 'LAZR', 'LI', 'LKCO', 'MARA', 'MSFT', 'MU', 'MVIS', 'NAKD', 'NNDM', 'NOVN', 'NVCN', 'NXTD', 'OCGN', 'OGI', 'ONTX', 'OPEN', 'PHUN', 'PLUG', 'PTE', 'PULM', 'PYPL', 'QTT', 'RCMT', 'RIOT', 'RKDA', 'SHIP', 'SIRI', 'SNDL', 'SOLO', 'SRNE', 'STAF', 'TELL', 'TLRY', 'TNXP', 'TRCH', 'TSLA', 'TXMD', 'TYME', 'VISL', 'VXRT', 'VYNE', 'XSPA', 'Z', 'ZNGA', 'ZSAN', 'ZYNE']
+    # dataframe = pd.read_csv('symbols.csv')
+    # return list(dataframe['Symbol'])
+
